3874f5b903632a2c8dec3984fdd1da86b9042eea
PrabhanshuAttri <contact@prabhanshu.com>
2020-04-11 14:41:53 -0400

    removal of unnecessary packages

--- installers/raspberry.sh
+++ installers/raspberry.sh
@@ -10,6 +10,9 @@ sudo apt-get install youtube-dl -y
 sudo apt-get install git htop zsh -y
 sudo apt-get install screen -y
 
+printf "\nPurging unnecessary packages\n"
+sudo apt-get remove --purge -y scratch2 minecraft-pi wolfram-engine sonic-pi dillo libreoffice libreoffice-avmedia-backend-gstreamer libreoffice-base libreoffice-base-core libreoffice-base-drivers libreoffice-calc libreoffice-common libreoffice-core libreoffice-draw libreoffice-gtk2 libreoffice-impress libreoffice-java-common libreoffice-math libreoffice-pi libreoffice-report-builder-bin libreoffice-sdbc-hsqldb libreoffice-writer squeak-vm squeak-plugins-scratch geany bluej scratch3 greenfoot-unbundled python-games
+
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
