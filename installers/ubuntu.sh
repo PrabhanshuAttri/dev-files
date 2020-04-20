d52003349bdcfc8e61b8fe852e5db892e539ff39
PrabhanshuAttri <contact@prabhanshu.com>
2020-04-19 23:23:16 -0400

    Added NVIDIA drivers and other related config

--- installers/ubuntu.sh
+++ installers/ubuntu.sh
@@ -12,6 +12,30 @@ sudo apt-get install ttf-mscorefonts-installer
 showStatus "Installing Update and Upgrade"
 sudo apt-get install ubuntu-restricted-extras software-properties-common -y && sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y
 
+showStatus "Adding NVIDIA package repositories"
+wget -nc https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo dpkg -i cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
+sudo apt-get update -y
+wget -nc http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+sudo apt install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb -y
+sudo apt-get update -y
+
+showStatus "Installing NVIDIA driver"
+sudo apt-get install --no-install-recommends nvidia-driver-430 -y
+# Reboot. Check that GPUs are visible using the command: nvidia-smi
+
+showStatus "Installing development and runtime libraries (~4GB)"
+sudo apt-get install -y --no-install-recommends \
+    cuda-10-1 \
+    libcudnn7=7.6.4.38-1+cuda10.1  \
+    libcudnn7-dev=7.6.4.38-1+cuda10.1
+
+showStatus "Installing TensorRT and libcudnn7"
+sudo apt-get install -y --no-install-recommends libnvinfer6=6.0.1-1+cuda10.1 \
+    libnvinfer-dev=6.0.1-1+cuda10.1 \
+    libnvinfer-plugin6=6.0.1-1+cuda10.1
+
 showStatus "Installing Google-Chrome"
 wget -nc --content-disposition https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
@@ -98,7 +122,7 @@ showStatus "Installing Cheese/Webcam app"
 sudo apt-get install cheese -y
 
 showStatus "Installing skype"
-wget https://go.skype.com/skypeforlinux-64.deb
+wget -nc https://go.skype.com/skypeforlinux-64.deb
 sudo apt-get install ./skypeforlinux-64.deb -y
 
 showStatus "Installing teamviewer"
@@ -162,15 +186,54 @@ Type=Application
 Categories=Development;
 EOL
 
+showStatus "Installing TLP for power saving"
+sudo apt-get install tlp tlp-rdw -y
+sudo tlp start
+
 showStatus "Removing guest account"
 sudo sh -c 'showStatus "[Seat:*]\nallow-guest=false\n" >/etc/lightdm/lightdm.conf.d/50--guest.conf'
 
 showStatus "Removing Amazon"
 echo 'Hidden=true' | cat /usr/share/applications/ubuntu-amazon-default.desktop - > ~/.local/share/applications/ubuntu-amazon-default.desktop
 
-showStatus "Installing TLP for power saving"
-sudo apt-get install tlp tlp-rdw -y
-sudo tlp start
+showStatus "Setting vim as default editor"
+sudo update-alternatives --set editor /usr/bin/vim.basic
+
+# Background color values
+# Default: Login Screen
+COLOR_HEX_ORIGINAL=2c001e
+# Default: GRUB
+COLOR_RGB_ORIGINAL=44,0,30
+# Default: Splash Screen
+COLOR_PERCENT_ORIGINAL="0.16, 0.00, 0.12"
+
+# New Values
+COLOR_HEX=222831
+COLOR_RGB=34,40,49
+COLOR_PERCENT="0.17, 0.00, 0.12"
+
+showStatus "Fixing Grub background"
+sudo sed -i "s/$COLOR_RGB_ORIGINAL/$COLOR_RGB/g" /usr/share/plymouth/themes/default.grub
+
+showStatus "Fixing Splash Screen"
+sudo sed -i "s/Window.SetBackgroundTopColor ($COLOR_PERCENT_ORIGINAL);/Window.SetBackgroundTopColor ($COLOR_PERCENT);/g" /usr/share/plymouth/themes/ubuntu-logo/ubuntu-logo.script
+sudo sed -i "s/Window.SetBackgroundBottomColor ($COLOR_PERCENT_ORIGINAL);/Window.SetBackgroundBottomColor ($COLOR_PERCENT);/g" /usr/share/plymouth/themes/ubuntu-logo/ubuntu-logo.script
+sudo update-initramfs -u
+
+showStatus "Fixing Login Screen"
+sudo sed -i "s/$COLOR_HEX_ORIGINAL/$COLOR_HEX/g" /usr/share/gnome-shell/theme/gdm3.css
+
+showStatus "Installing GRUB theme"
+cd /usr/share/grub/
+sudo mkdir themes
+cd themes
+sudo git clone https://github.com/lfelipe1501/Atomic-GRUB2-Theme.git
+sudo mv Atomic-GRUB2-Theme/Atomic ./
+sudo rm -rf Atomic-GRUB2-Theme
+sudo grep -v GRUB_THEME < /etc/default/grub > /tmp/clean_grub
+sudo mv /tmp/clean_grub /etc/default/grub
+sudo echo "GRUB_THEME=/usr/share/grub/themes/Atomic/theme.txt" >> /etc/default/grub
+sudo update-grub
 
 showStatus "Remove duplicate sources"
 sudo apt-get install python3-apt python3-regex -y
@@ -178,4 +241,4 @@ curl -LO https://github.com/davidfoerster/aptsources-cleanup/releases/download/v
 sudo python3 -OEs aptsources-cleanup.zip
 
 showStatus "Update everything"
-sudo apt-get install ubuntu-restricted-extras -y && sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y
+sudo apt-get install ubuntu-restricted-extras -y && sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get autoremove -y
